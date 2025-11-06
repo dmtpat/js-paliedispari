b40d8b9dcd4922fb79cd10344e259a16c7252d75
Pat <dmtpatricica@email.com>
2025-11-06 17:03:16 +0100

    1 - while loops  + minor

--- palindroma/main.js
+++ palindroma/main.js
@@ -18,7 +18,7 @@ function wordReverse(word) {
 const parola = prompt("Inserisci la parola che vuoi controllare").toUpperCase();
 
 if (parola === wordReverse(parola)) {
-    alert(`La parola ${parola} è PALINDROMA`)
+    alert(`La parola "${parola}" è PALINDROMA`)
 } else {
-    alert(`La parola ${parola} NON è PALINDROMA, è diversa da ${wordReverse(parola)}`)
+    alert(`La parola "${parola}" NON è PALINDROMA, è diversa da ${wordReverse(parola)}`)
 }
--- pariDispari/main.js
+++ pariDispari/main.js
@@ -52,25 +52,33 @@ let puntata = prompt("Scegli se puntare su pari o su dispari").toUpperCase();
 let valore = 0;
 let valorePc = randomIntegerNumber(1, 5);
 
-if (puntata != `PARI` || puntata != `DISPARI`) {
-    puntata = parseInt(prompt("Scegli un valore tra PARI O DISPARI"));
+while(puntata != `PARI` && puntata != `DISPARI`){
+    puntata = prompt("Scegli un valore tra PARI o DISPARI").toUpperCase();
 }
-if (puntata != `PARI` || puntata != `DISPARI`) {
-    if (randomIntegerNumber(0, 1) === 0) {
-        puntata = `PARI`;
-    } else {
-        puntata = `DISPARI`;
-    }
-    alert(`I due tentativi precedenti non sono andati a buon fine. Ho scelto ${puntata} per te`);
-} //ᐠ｡ꞈ｡ᐟ\ 
+    
+// if (puntata != `PARI` || puntata != `DISPARI`) {
+//     puntata = parseInt(prompt("Scegli un valore tra PARI O DISPARI"));
+// }
+// if (puntata != `PARI` || puntata != `DISPARI`) {
+//     if (randomIntegerNumber(0, 1) === 0) {
+//         puntata = `PARI`;
+//     } else {
+//         puntata = `DISPARI`;
+//     }
+//     alert(`I due tentativi precedenti non sono andati a buon fine. Ho scelto ${puntata} per te`);
+// } //ᐠ｡ꞈ｡ᐟ\ 
 valore = parseInt(prompt("Inserisci il tuo numero tra 1 e 5"));
-if (!isNumberInRange(valore, 1, 5)) {
-    valore = parseInt(prompt("Inserisci un nuovo valore accettabile( un numero intero compreso tra 1 e 5)")); 
+while(!isNumberInRange(valore, 1, 5)){
+    valore = parseInt(prompt("Inserisci un nuovo valore accettabile( un numero intero compreso tra 1 e 5)"));
 }
-if (!isNumberInRange(valore, 1, 5)) {
-    valore = randomIntegerNumber(1, 5);
-    alert( `I due tentativi precedenti non sono andati a buon fine. Ho scelto un numero random tra 1 e 5 per te. Ho scelto ${valore}.`)
-}
+
+// if (!isNumberInRange(valore, 1, 5)) {
+//     valore = parseInt(prompt("Inserisci un nuovo valore accettabile( un numero intero compreso tra 1 e 5)")); 
+// }
+// if (!isNumberInRange(valore, 1, 5)) {
+//     valore = randomIntegerNumber(1, 5);
+//     alert( `I due tentativi precedenti non sono andati a buon fine. Ho scelto un numero random tra 1 e 5 per te. Ho scelto ${valore}.`)
+// }
 if (isPari((valore + valorePc)) && puntata === `PARI`) {
     alert(`Hai Vinto! IL tuo valore è ${valore} il mio è ${valorePc} il risultato è ${puntata}`);
 } else if (!isPari((valore + valorePc)) && puntata === `DISPARI`) {
