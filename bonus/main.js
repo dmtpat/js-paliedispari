5f596c129ad7b847f51ab4b91e4fb62eba9ab93b
Pat <dmtpatricica@email.com>
2025-11-06 17:19:04 +0100

    bonus - first commit

---
+++ bonus/main.js
@@ -0,0 +1,11 @@
+// BONUS
+
+/**
+ *  Restituisce una stringa con l'iniziale di ogni parola maiuscola
+ *  @param {string} text  La stringa da modificare
+ *  @return {string} La nuova stringa con le iniziali maiuscole
+ */
+function capitalizeText(text) {
+    
+
+}
